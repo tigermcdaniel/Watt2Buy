a028e7cba400a051c8375b1a1dd3566618dc1598
Hung Bui <hubu6266@colorado.edu>
2020-10-30 16:47:07 -0600

    Database Script & Model

---
+++ WATT2BUY.sql
@@ -0,0 +1,178 @@
+CREATE DATABASE WATT2BUY;
+
+USE WATT2BUY;
+
+CREATE TABLE USER (
+  id INT NOT NULL AUTO_INCREMENT, -- unique id
+  firstName VARCHAR(50),
+  lastName VARCHAR(50),
+  mobile VARCHAR(15),
+  email VARCHAR(50),
+  password VARCHAR(32),
+  role VARCHAR(10), -- admin / manager / vendor / customer
+  registeredAt DATETIME NOT NULL,
+  lastLogin DATETIME,
+  PRIMARY KEY (id)
+  );
+  
+  
+  
+  CREATE TABLE PRODUCT (
+  id INT NOT NULL AUTO_INCREMENT,
+  userId INT NOT NULL, -- the user that links with the product
+  title VARCHAR(75) NOT NULL,
+  description TEXT NULL,
+  type INT NOT NULL DEFAULT 0,
+  sku VARCHAR(50) NOT NULL, -- stock keeping unit
+  price FLOAT NOT NULL DEFAULT 0,
+  discount FLOAT NOT NULL DEFAULT 0,
+  quantity SMALLINT(6) NOT NULL DEFAULT 0, -- available quantity
+  available BOOLEAN NOT NULL,
+  createdAt DATETIME NOT NULL,
+  updatedAt DATETIME NULL DEFAULT NULL,
+  publishedAt DATETIME NULL DEFAULT NULL,
+  saleStartsAt DATETIME NULL DEFAULT NULL,
+  saleEndsAt DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_product_user FOREIGN KEY (userId) 
+  REFERENCES USER (id)
+);
+  
+  
+CREATE TABLE CATEGORY (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(75) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE PRODUCT_CATEGORY (
+  productId INT NOT NULL,
+  categoryId INT NOT NULL,
+  PRIMARY KEY (productId, categoryId),
+  CONSTRAINT fk_pc_product
+    FOREIGN KEY (productId)
+    REFERENCES PRODUCT (id),
+  CONSTRAINT fk_pc_category
+    FOREIGN KEY (categoryId)
+    REFERENCES category (id)
+);
+
+
+CREATE TABLE CART (
+  id INT NOT NULL AUTO_INCREMENT,
+  userId INT NULL DEFAULT NULL,
+  sessionId VARCHAR(100) NOT NULL, -- session id that links with the cart
+  token VARCHAR(100) NOT NULL, -- unique token that link with the cart over multiple sessions
+  status VARCHAR(50) NOT NULL, -- NEW / CART / CHECKOUT / PAID / COMPLETE
+  firstName VARCHAR(50) NULL DEFAULT NULL,
+  lastName VARCHAR(50) NULL DEFAULT NULL,
+  mobile VARCHAR(15) NULL,
+  email VARCHAR(50) NULL,
+  address1 VARCHAR(50) NULL DEFAULT NULL,
+  address2 VARCHAR(50) NULL DEFAULT NULL,
+  city VARCHAR(50) NULL DEFAULT NULL,
+  state VARCHAR(50) NULL DEFAULT NULL,
+  country VARCHAR(50) NULL DEFAULT NULL,
+  createdAt DATETIME NOT NULL,
+  updatedAt DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_cart_user
+    FOREIGN KEY (userId)
+    REFERENCES USER (ID)
+);
+
+
+
+CREATE TABLE CART_ITEM (
+  id INT NOT NULL AUTO_INCREMENT,
+  productId INT NOT NULL,
+  cartId INT NOT NULL,
+  sku VARCHAR(100) NOT NULL,
+  price FLOAT NOT NULL,
+  discount FLOAT NOT NULL DEFAULT 0,
+  quantity INT(6) NOT NULL DEFAULT 0,
+  active BOOLEAN NOT NULL,
+  createdAt DATETIME NOT NULL,
+  updatedAt DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_cart_item_product
+    FOREIGN KEY (productId)
+    REFERENCES PRODUCT (ID),
+  CONSTRAINT fk_cart_item_cart
+    FOREIGN KEY (cartId)
+    REFERENCES CART (ID)
+);
+
+
+
+CREATE TABLE WATT2BUY.ORDER (
+  id INT NOT NULL AUTO_INCREMENT,
+  userId INT NULL DEFAULT NULL,
+  sessionId VARCHAR(100) NOT NULL,
+  token VARCHAR(100) NOT NULL,
+  status INT(6) NOT NULL DEFAULT 0,
+  subTotal FLOAT NOT NULL DEFAULT 0,
+  itemDiscount FLOAT NOT NULL DEFAULT 0,
+  tax FLOAT NOT NULL DEFAULT 0,
+  shipping FLOAT NOT NULL DEFAULT 0,
+  total FLOAT NOT NULL DEFAULT 0,
+  discount FLOAT NOT NULL DEFAULT 0,
+  grandTotal FLOAT NOT NULL DEFAULT 0,
+  firstName VARCHAR(50) NULL DEFAULT NULL,
+  lastName VARCHAR(50) NULL DEFAULT NULL,
+  mobile VARCHAR(15) NULL,
+  email VARCHAR(50) NULL,
+  address1 VARCHAR(50) NULL DEFAULT NULL,
+  address2 VARCHAR(50) NULL DEFAULT NULL,
+  city VARCHAR(50) NULL DEFAULT NULL,
+  state VARCHAR(50) NULL DEFAULT NULL,
+  country VARCHAR(50) NULL DEFAULT NULL,
+  createdAt DATETIME NOT NULL,
+  updatedAt DATETIME NULL DEFAULT NULL,
+  content TEXT NULL DEFAULT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_order_user
+    FOREIGN KEY (userId)
+    REFERENCES USER (id)
+);
+
+
+CREATE TABLE ORDER_ITEM (
+  id INT NOT NULL AUTO_INCREMENT,
+  productId INT NOT NULL,
+  orderId INT NOT NULL,
+  sku VARCHAR(100) NOT NULL,
+  price FLOAT NOT NULL DEFAULT 0,
+  discount FLOAT NOT NULL DEFAULT 0,
+  quantity SMALLINT(6) NOT NULL DEFAULT 0,
+  createdAt DATETIME NOT NULL,
+  updatedAt DATETIME NULL DEFAULT NULL,
+  content TEXT NULL DEFAULT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_order_item_product
+    FOREIGN KEY (productId)
+    REFERENCES PRODUCT (id),
+  CONSTRAINT fk_order_item_order
+  FOREIGN KEY (orderId)
+  REFERENCES WATT2BUY.ORDER (id)
+);
+
+
+
+
+CREATE TABLE TRANSACTION (
+  id INT NOT NULL AUTO_INCREMENT,
+  userId INT NOT NULL,
+  orderId INT NOT NULL,
+  type VARCHAR(50) NOT NULL DEFAULT 0, -- credit / debit / paypal 
+  status VARCHAR(50) NOT NULL DEFAULT 0, -- NEW / CANCELLED / FAILED / PENDING / DECLINED / REJECTED / COMPLETE
+  createdAt DATETIME NOT NULL,
+  updatedAt DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_transaction_user
+    FOREIGN KEY (userId)
+    REFERENCES USER (id)
+);
+
+

a96a102811d42998b9d38e8e028d42af7fbeac1d
vava3772 <66335300+vava3772@users.noreply.github.com>
2020-11-20 10:33:26 -0700

    Budget test added to Results.js -Vanessa
    Prints results correctly however there is an issue with reading values from the survey. 
    
    Error message --Uncaught TypeError: Cannot read property 'value' of nullat budgetTest (Results.js:16)
    Code line -- inBudget = document.getElementById("budget").value;
    
    Results.js was included at the bottom of both Survey and results.

--- All Project Code/watt2buy/js/Results.js
+++ All Project Code/watt2buy/js/Results.js
@@ -1,13 +1,34 @@
-var mysql = require('WATT2BUY');
+//var mysql = require('WATT2BUY');
+
+console.log("Test1");
+window.onload =budgetTest;
+console.log("Test2");
+//just to check if connection via budget
+var resultTherm= "<div style='width: 33%;'><div class='card'><img class='card-img-top' src='../img/smartTherm.jpg'><div class='card-body'><h3 class='card-title'>Smart Thermostat</h3><p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p></div><ul class='list-group list-group-flush'><li class='list-group-item'>Investment Cost: $</li></ul></div></div>";
+var resultShade= "<div style='width: 33%;'><div class='card'><img class='card-img-top' src='../img/smartShades.JPG'><div class='card-body'><h3 class='card-title'>Smart Shades</h3><p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p></div><ul class='list-group list-group-flush'><li class='list-group-item'>Investment Cost: $</li></ul></div></div>";
+var resultLight= "<div style='width: 33%;'><div class='card'><img class='card-img-top' src='../img/smartLighting.jpg'><div class='card-body'><h3 class='card-title'>Smart Lighting</h3><p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p></div><ul class='list-group list-group-flush'><li class='list-group-item'>Investment Cost: $</li></ul></div></div>";
+
+document.getElementById("resultsAfterSurvey").innerHTML = resultTherm + resultShade + resultLight;
+var inBudget;
+
+function budgetTest(){
+  console.log("Test3");
+  inBudget = document.getElementById("budget").value;
+  console.log(inBudget);
+  if (inBudget <=1000){
+    document.getElementById("resultsAfterSurvey").innerHTML = resultTherm + resultShade + resultLight;
+  }
+}
+
 
 function addTech(){
     var Q1 = document.getElementById("Question1").value;
     var Q2 = document.getElementById("Question2").value;
     // Continue with this once survey is finalized
 
-    
 
-    //add User to WATT2BUT.SQL USER table 
+
+    //add User to WATT2BUT.SQL USER table
     var mysql = require('WATT2BUY');
 
     var con = mysql.createConnection({

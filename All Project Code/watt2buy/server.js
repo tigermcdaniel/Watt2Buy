cccd9bff8524cd7e1503f45f2005a68a9708ec84
tigermcdaniel <tigermcdaniel@github.com>
2020-12-03 21:42:57 -0800

    user may save results to database

--- All Project Code/watt2buy/server.js
+++ All Project Code/watt2buy/server.js
@@ -109,8 +109,7 @@ app.get('/nodejs/saveResults', function(req, res) {
     var results = req.query.results;
     var userName = req.query.userName;
     
-    var insert_statement = "INSERT INTO public.\"RESULTS\"(username, results) VALUES('" + userName + "','"
-                 + "','" + results +  "');";
+    var insert_statement = "INSERT INTO public.\"RESULTS\"(username, results) VALUES('" + userName + "','" + results +  "');";
 
 
 	db.task('get-everything', task => {
